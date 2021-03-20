79b4d6dd4396c023c5547ace5a04c7d2d0c1cd3f
winkadd <78963510+winkadd@users.noreply.github.com>
2021-03-20 21:01:49 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int max(int arr[][2])
+//{
+//	int i, j;
+//	int max = arr[0][0];
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 2; j++)
+//		{
+//			if (arr[i][j]>max)
+//				max = arr[i][j];
+//		}
+//		
+//	}
+//
+//	return(max);
+//}
+//int main()
+//{
+//	int arr[][2] = { { 25, 56, }, { 44, 32 }, {33,25} };
+//	printf("%d\n", max(arr));
+//	return 0;
+//}
+//void reverse(int arr[][3])
+//{	
+//	int arr1[3][3];
+//	int i,j = 0;
+//	for ( i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 3; j++)
+//		{
+//			arr1[i][j] = arr[j][i];
+//			printf(" %d ",arr1[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	
+//
+//}
+//int main()
+//{
+//	void reverse(int arr[][3]);
+//	int arr[][3] = { { 1, 1, 1 }, { 2, 2, 2 }, { 3, 3, 3 } };
+//	reverse(arr);
+//	return 0;
+//}
